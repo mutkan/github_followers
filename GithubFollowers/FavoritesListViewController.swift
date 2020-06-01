950cc0522e140ff74697b3ae11e647fb66df71c2
mutlu <mutkanx@gmail.com>
2020-06-01 02:20:18 -0500

    Add started section

---
+++ GithubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 1.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
--- GithubFollowers/SceneDelegate.swift
+++ GithubFollowers/SceneDelegate.swift
@@ -17,9 +17,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
+    }
+    
+    func createTabBar() -> UITabBarController{
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .red
+        tabBar.viewControllers = [createSearchViewController(), createFavoritesListViewController()]
+        return tabBar
+    }
+    
+    func createSearchViewController() -> UINavigationController{
+        let search = SearchViewController()
+        search.title = "Search"
+        search.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: search)
     }
 
+    func createFavoritesListViewController() -> UINavigationController{
+        let favorites = FavoritesListViewController()
+        favorites.title = "Favorites"
+        favorites.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favorites)
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
