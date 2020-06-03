53a660419863ebd55eb4def60c64e4f7118c2b54
mutlu <mutkanx@gmail.com>
2020-06-03 01:34:25 -0500

    Add custom title label class

---
+++ GithubFollowers/CustomViews/CustomTitleLabel.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTitleLabel.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 3.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class CustomTitleLabel : UILabel{
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
