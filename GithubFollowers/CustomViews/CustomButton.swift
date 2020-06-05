c5dafe37f23b8e6a5a703ec4c13e68a9d3372d32
mutlu <mutkanx@gmail.com>
2020-06-04 23:23:12 -0500

    Add custom button

---
+++ GithubFollowers/CustomViews/CustomButton.swift
@@ -0,0 +1,30 @@
+//
+//  CustomizeButton.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 4.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton{
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .highlighted)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
