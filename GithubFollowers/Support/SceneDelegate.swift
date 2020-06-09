52c2f243388f0fada097f71e735652e125182956
mutlu <mutkanx@gmail.com>
2020-06-08 23:42:31 -0500

    Change navigation bar tint color to green

--- GithubFollowers/Support/SceneDelegate.swift
+++ GithubFollowers/Support/SceneDelegate.swift
@@ -48,6 +48,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         return UINavigationController(rootViewController: favorites)
     }
     
+    func configureNavigationBar(){
+        UINavigationBar.appearance().tintColor = .systemGreen
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
