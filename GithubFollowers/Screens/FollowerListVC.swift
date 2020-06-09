b70a9a57af95b82e30e026c311238b61f2424033
mutlu <mutkanx@gmail.com>
2020-06-08 23:39:59 -0500

    Add follower list vc

---
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 8.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController{
+    var userName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
