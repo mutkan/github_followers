a5f4c7013e2e5332beaf4370727eb5b0229816dc
mutlu <mutkanx@gmail.com>
2020-06-12 00:57:23 -0500

    Revision

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class FollowerListVC: UIViewController{
-    var userName: String?
+    var userName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: userName ?? "", page: 1){ result in
+        NetworkManager.shared.getFollowers(for: userName, page: 1){ result in
             switch result {
                 case .success(let followers):
                     print("Followes count: \(followers.count)")
