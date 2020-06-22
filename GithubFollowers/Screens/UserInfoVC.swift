05a83faed5dc64fd10192e020ac4d679e30994da
mutlu <mutkanx@gmail.com>
2020-06-21 23:29:16 -0500

    Add user info screen

---
+++ GithubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfoVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 21.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
