71e996b58a2c796b5100e4057b9e1183178a1f9a
mutlu <mutkanx@gmail.com>
2020-06-23 23:45:15 -0500

    Add constants file

---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 23.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
