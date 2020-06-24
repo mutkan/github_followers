8ad5f3af2ed79da7d62e915dccada981d405c234
mutlu <mutkanx@gmail.com>
2020-06-23 23:38:09 -0500

    Add user info header vc

---
+++ GithubFollowers/CustomViews/Views/UserInfoHeaderVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoHeaderVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 23.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoHeaderVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
