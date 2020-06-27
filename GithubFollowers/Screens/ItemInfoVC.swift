c53b35a23091691baac71ab083f47dae84c8123b
mutlu <mutkanx@gmail.com>
2020-06-26 23:48:22 -0500

    Set up item info view controller

---
+++ GithubFollowers/Screens/ItemInfoVC.swift
@@ -0,0 +1,68 @@
+//
+//  ItemInfoVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 26.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class ItemInfoVc : UIViewController{
+    var mUser: User? = nil
+    
+    let stackView = UIStackView()
+    let itemInfoViewOne = CustomItemInfoView()
+    let itemInfoViewTwo = CustomItemInfoView()
+    let actionButton = CustomButton()
+
+    
+    init(user: User){
+        super.init(nibName: nil, bundle: nil)
+        self.mUser = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        configureStackView()
+        layoutUI()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+   private func configureStackView(){
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+    
+    private func layoutUI(){
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding)
+        ])
+    }
+    
+}
