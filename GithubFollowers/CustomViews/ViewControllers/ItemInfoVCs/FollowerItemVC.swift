27eeee443973bbbaccfa81ced4e593e6274599a8
mutlu <mutkanx@gmail.com>
2020-06-27 23:21:14 -0500

    Create item vcs

---
+++ GithubFollowers/CustomViews/ViewControllers/ItemInfoVCs/FollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  FollowerItemVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 27.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import Foundation
+
+class FollowerItemVC : ItemInfoVc{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: mUser?.followers ?? 0)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: mUser?.following ?? 0)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GithubFollowers/CustomViews/ViewControllers/ItemInfoVCs/RepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  RepoItemVC.swift
+//  GithubFollowers
+//
+//  Created by Mutlu Celep on 27.06.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import Foundation
+
+class RepoItemVC: ItemInfoVc {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    func configureItems(){
+        itemInfoViewOne.set(itemInfoType: .gists, withCount: mUser?.publicGists ?? 0)
+        itemInfoViewTwo.set(itemInfoType: .repos, withCount: mUser?.publicRepos ?? 0)
+        actionButton.set(backgroundColor: .systemRed, title: "Github Profile")
+    }
+}
