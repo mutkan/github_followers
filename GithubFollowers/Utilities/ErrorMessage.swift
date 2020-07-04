32cf2e0fde5040317c9c733807a29adfc108b5c4
mutlu <mutkanx@gmail.com>
2020-07-04 02:59:37 -0500

    Re-indent

--- GithubFollowers/Utilities/ErrorMessage.swift
+++ GithubFollowers/Utilities/ErrorMessage.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 enum CustomErr: String, Error{
-    case invalidUsername = "This username created an invalid request. Please try again."
-    case unableToComplete = "Unable to complete your request. Please check your internet connection"
-    case invalidResponse = "Invalid response from the server. Please try again."
-    case invalidData = "The data recieved from the server was invalid. Please try again"
-    case unableToFavorites = "There was an error favoriting this user. Please try again."
+    case invalidUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data recieved from the server was invalid. Please try again"
+    case unableToFavorites  = "There was an error favoriting this user. Please try again."
     case alreadyInFavorites = "You've already favorited this user.You must REALLY like them!"
 }
